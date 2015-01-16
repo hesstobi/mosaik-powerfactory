de004c1baf27bc6c11d0e4c472a1b2e3501181bc
Tobias Heß <tobias.hess@tu-dresden.de>
2015-01-16 13:33:29 +0100

    Add init methods

--- mosaik-powerfactory.py
+++ mosaik-powerfactory.py
@@ -7,7 +7,7 @@ import sys
 import os
 os.environ["PATH"] = "C:\\Program Files\\DIgSILENT\\PowerFactory 15.2;" + os.environ["PATH"]
 sys.path.append("C:\\Program Files\\DIgSILENT\\PowerFactory 15.2\\python\\3.4")
-import powerfactory as pf
+import powerfactory
 
 import mosaik_api
 
@@ -22,11 +22,31 @@ META = {
 }
 
 
-class PowerFactorySimulator(mosaik_api.Simulator)
+class PowerFactorySimulator(mosaik_api.Simulator):
+    def __init__(self):
+        # Init the Metadata in the Super Class
+        super().__init__(META)
+        # Init Power Facotry
+        self.pf = powerfactory.GetApplication()
+        if self.pf is None:
+            raise Exception("Starting PowerFactory application in engine mode failed")
 
+        # Set a eid_prefix
+        self.eid_prefix= 'PF_'
 
+        # Create the entities Container
+        self.entities = {}  # Maps EIDs to model indices in PowerFactory
 
-
+    def init(self, sid, project_name ,eid_prefix=None):
+        # Option overwrite the eid_prefix
+        if eid_prefix is not None:
+            self.eid_prefix = eid_prefix
+        # Activate project in powerfactory
+        if project_name is None:
+            raise Exception("You have to provide the project_name for PowerFactory")
+        self.pf.ActivateProject(project_name)
+        # this methode has to return the meta dict
+        return self.meta
 
 
 
