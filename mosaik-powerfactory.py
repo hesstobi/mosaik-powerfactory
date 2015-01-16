a0a7acf96927859f8b7df059f6df0e224b8f1568
Tobias Heß <tobias.hess@tu-dresden.de>
2015-01-16 13:12:23 +0100

    Class definition and main function

--- mosaik-powerfactory.py
+++ mosaik-powerfactory.py
@@ -5,8 +5,27 @@ Mosaik interface for Digisilent Powerfactory
 """
 import sys
 import os
-os.environ["PATH"] = 'C:\Program Files\DIgSILENT\PowerFactory 15.2;' + os.environ["PATH"]
+os.environ["PATH"] = "C:\\Program Files\\DIgSILENT\\PowerFactory 15.2;" + os.environ["PATH"]
 sys.path.append("C:\\Program Files\\DIgSILENT\\PowerFactory 15.2\\python\\3.4")
 import powerfactory as pf
 
 import mosaik_api
+
+
+class PowerFactorySimulator(mosaik_api.Simulator)
+
+
+
+
+
+
+
+
+
+# Make it executable
+def main():
+    return mosaik_api.start_simulation(ExampleSim())
+
+
+if __name__ == '__main__':
+    main()
