06cd7e6f1d737bfcc4fceaec87667a85c2279152
Tobias Heß <tobias.hess@tu-dresden.de>
2015-02-09 13:40:21 +0100

    Add Documentation to mosiak_tools

--- mosaik_tools.py
+++ mosaik_tools.py
@@ -3,11 +3,35 @@ import mosaik.scenario
 
 
 def child_with_eid(self,eid):
+    """Returns the child with the given eid
+
+    This method extends the mosaik.scenario.Entity class. It returns the a child
+    of the entity with the given eid
+
+    Args:
+        eid: The eid for the child
+
+    Returns:
+        The child entity
+
+    """
     children = self.children
     child = [e for e in children if e.eid == eid]
     return child[0]
 
 def children_of_model(self,model):
+    """Returns all children of the given model
+
+    This method extends the mosaik.scenario.Entity class. It returns all children
+    of the given model of the entity.
+
+    Args:
+        model: The model of the children
+
+    Returns:
+        A list of entities.
+
+    """
     children = self.children
     children = [e for e in children if e.type == model]
     return children
